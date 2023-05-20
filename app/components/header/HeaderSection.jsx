3013dc6faf20dad65a12e3e35a461ef86d157675
themuuln <zerone.offical@gmail.com>
2023-05-20 20:27:17 +0800

    Header Added

---
+++ app/components/header/HeaderSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderSection = () => {
+  return (
+    <header className='h-10 bg-[#f6f5f0] text-brand'>HeaderSection</header>
+  )
+}
+
+export default HeaderSection

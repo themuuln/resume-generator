16f5484fa2e156e2acc619dd8a6cd027fa17d32b
themuuln <zerone.offical@gmail.com>
2023-05-21 00:24:24 +0800

    Footer, Header Added

---
+++ app/components/footer/FooterSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FooterSection = () => {
+  return (
+    <footer className="py-4 bg-gray-800">
+      <div className="container mx-auto text-center text-white">
+        <p className="text-sm md:text-base">
+          &copy; 2023 Ajildori. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterSection;
--- app/components/header/HeaderSection.jsx
+++ app/components/header/HeaderSection.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HeaderSection = () => {
   return (
-    <header className='h-10 bg-[#f6f5f0] text-brand'>HeaderSection</header>
+    <header className='h-10 bg-[#f6f5f0] flex justify-center text-brand'>
+      <div className="container">Hi</div>
+    </header>
   )
 }
 

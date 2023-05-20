8960232d1ea0afbb8148b236f716a8c8205d31a7
themuuln <zerone.offical@gmail.com>
2023-05-20 20:26:35 +0800

    Styles Added

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import Button from "./components/Button";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <p>Hi Welcome to CV/Resume Generator</p>
-      <button>
+      <Button>
         <Link href={"/generator"}>
           Generate
         </Link>
-      </button>
+      </Button>
     </main>
   );
 }

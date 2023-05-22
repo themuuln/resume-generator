1bc8aea3953013f8984c02d6494c04a9ac237d04
themuuln <zerone.offical@gmail.com>
2023-05-22 20:59:06 +0800

    Header Context Updated

--- app/components/header/HeaderSection.jsx
+++ app/components/header/HeaderSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderSection = () => {
   return (
-    <header className='h-10 bg-[#f6f5f0] flex justify-center text-brand'>
-      <div className="container">Hi</div>
+    <header className='h-10 bg-[#f6f5f0] flex justify-center items-center text-brand'>
+      <div className="container">Hi this is your fucking header bro, its not done yet.</div>
     </header>
   )
 }
